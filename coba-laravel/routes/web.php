0a292756e0ebbe2390f0f6375283612b6ec36e86
nadliw090801 <muhammadwildan334@gmail.com>
2021-10-04 07:25:30 +0700

    Mempelajari Struktur Folder, Routes & View pada Laravel 8

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Wildhan",
+        "email" => "muhammadwildhan@unpas.ac.id",
+        "image" => "193040009.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
