c89033873e1142ade279551e143d2a54290005b6
nadliw090801 <muhammadwildan334@gmail.com>
2021-10-27 10:38:30 +0700

    Mempelajari View Login & Registration pada Laravel

--- coba-laravel/resources/views/layouts/main.blade.php
+++ coba-laravel/resources/views/layouts/main.blade.php
@@ -8,6 +8,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
+    {{-- Bootstrap Icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+
+    {{-- My Style --}}
+    <link rel="stylesheet" href="css/style.css">
+
     <title>WPU Blog | {{ $title }}</title>
   </head>
   <body>
